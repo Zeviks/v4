1b3325d7837137499a8fa29af3caa3e5d2fc477b
Natanael Pacheco <natepacheco716@gmail.com>
2021-10-25 09:55:00 -0500

    Site Completed

--- src/config.js
+++ src/config.js
@@ -4,24 +4,24 @@ module.exports = {
   socialMedia: [
     {
       name: 'GitHub',
-      url: 'https://github.com/bchiang7',
+      url: 'https://github.com/Zeviks',
     },
-    {
-      name: 'Instagram',
-      url: 'https://www.instagram.com/bchiang7',
-    },
+    // {
+    //   name: 'Instagram',
+    //   url: 'https://www.instagram.com/_natez_/',
+    // },
     {
       name: 'Twitter',
       url: 'https://twitter.com/bchiang7',
     },
     {
       name: 'Linkedin',
-      url: 'https://www.linkedin.com/in/bchiang7',
+      url: 'https://www.linkedin.com/in/natanaelpachecorobles/',
     },
-    {
-      name: 'Codepen',
-      url: 'https://codepen.io/bchiang7',
-    },
+    // {
+    //   name: 'Codepen',
+    //   url: 'https://codepen.io/bchiang7',
+    // },
   ],
 
   navLinks: [
