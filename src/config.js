a2081e2868703040d019015f92d9412e159c3fc9
Natanael Pacheco <natepacheco716@gmail.com>
2021-10-25 10:18:18 -0500

    Test

--- src/config.js
+++ src/config.js
@@ -46,7 +46,7 @@ module.exports = {
   colors: {
     green: '#64ffda',
     navy: '#0a192f',
-    darkNavy: '#020c1b',
+    darkNavy: 'rgba(2, 12, 27, 0.7)',
   },
 
   srConfig: (delay = 200, viewFactor = 0.25) => ({
